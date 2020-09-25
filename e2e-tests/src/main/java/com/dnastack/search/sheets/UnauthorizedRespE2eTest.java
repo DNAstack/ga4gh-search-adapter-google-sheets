e0bc6d64077d02a6750c0c6c89505e143886f931
Brian Li <guo@dnastack.com>
2020-09-25 09:33:29 -0400

    [#172921509] Clean up some code and formatting

--- e2e-tests/src/main/java/com/dnastack/search/sheets/UnauthorizedRespE2eTest.java
+++ e2e-tests/src/main/java/com/dnastack/search/sheets/UnauthorizedRespE2eTest.java
@@ -1,51 +1,53 @@
 package com.dnastack.search.sheets;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
+/**
+ * Ensure that all secured endpoints require a valid bearer token.
+ */
 public class UnauthorizedRespE2eTest extends BaseE2eTest {
 
-    // TODO: Probably don't need to test against the actual sheet
+    private final String sheetIDEndpoint = "/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits";
 
-    /**
-     * Ensure that all secured endpoints require a valid bearer token.
-     */
     @Test
-    public void getWithMalformedTokenShouldGive400() {
-        getEndpointWithoutTokenAndEnsure400Response("/tables");
-        getEndpointWithoutTokenAndEnsure400Response("/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits/info");
-        getEndpointWithoutTokenAndEnsure400Response("/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits/data-model");
-        getEndpointWithoutTokenAndEnsure400Response("/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits/data");
+    public void getShouldGive400WhenAuthHeaderIsMissing() {
+        getWithoutAuthHeaderAndCheckFor400("/tables");
+        getWithoutAuthHeaderAndCheckFor400(sheetIDEndpoint + "/info");
+        getWithoutAuthHeaderAndCheckFor400(sheetIDEndpoint + "/data-model");
+        getWithoutAuthHeaderAndCheckFor400(sheetIDEndpoint + "/data");
     }
 
     @Test
-    public void getWithoutTokenShouldGive401() {
-        getEndpointWithMalformedTokenAndEnsure401Response("/tables");
-        getEndpointWithMalformedTokenAndEnsure401Response("/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits/info");
-        getEndpointWithMalformedTokenAndEnsure401Response("/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits/data-model");
-        getEndpointWithMalformedTokenAndEnsure401Response("/table/17LzrJJXKraCCsfCZJ1FqCsMAAoYTcntkpS5gSvOsx1w:Fruits/data");
+    public void getShouldGive401WhenInvalidTokenUsed() {
+        getWithInvalidTokenAndCheckFor401("/tables");
+        getWithInvalidTokenAndCheckFor401(sheetIDEndpoint + "/info");
+        getWithInvalidTokenAndCheckFor401(sheetIDEndpoint + "/data-model");
+        getWithInvalidTokenAndCheckFor401(sheetIDEndpoint + "/data");
     }
 
-    private void getEndpointWithoutTokenAndEnsure400Response(String endpoint) {
+    private void getWithoutAuthHeaderAndCheckFor400(String endpoint) {
         given()
             .log().uri()
-            .when()
-        .get(endpoint)
-            .then()
-            .statusCode(400)
-            .body("message", CoreMatchers.equalTo("Missing request header 'authorization' for method parameter of type String"));
-    }
-
-    private void getEndpointWithMalformedTokenAndEnsure401Response(String endpoint) {
-        given()
-            .log().uri()
-            .header("Authorization", "Bearer " + "Malformed_bearer")
-            .when()
+        .when()
             .get(endpoint)
-            .then()
+        .then()
+            .statusCode(400)
+            .body("message", equalTo("Missing request header 'authorization' for method parameter of type String"));
+    }
+
+    private void getWithInvalidTokenAndCheckFor401(String endpoint) {
+        given()
+            .log().uri()
+            .header("Authorization", "Bearer " + "A_malformed_bearer_token")
+        .when()
+            .get(endpoint)
+        .then()
             .statusCode(401)
-            .header("WWW-Authenticate", "[Bearer realm=\"https://accounts.google.com/\", error=invalid_token]");
+            // Attempt at future-proofing since the header is from Google. Avoid exact match of full string.
+            .header("WWW-Authenticate", containsStringIgnoringCase("Bearer"))
+            .header("WWW-Authenticate", containsStringIgnoringCase("invalid_token"));
     }
 }
